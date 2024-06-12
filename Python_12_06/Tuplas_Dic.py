ae4a30a8bc591641b17c1ab7aecf80d3f46d99a8
rhuan-rgb <rhuan.lima.cmd@gmail.com>
2024-06-12 10:46:48 -0300

    commit pedido pelo Arduino

--- Python_12_06/Tuplas_Dic.py
+++ Python_12_06/Tuplas_Dic.py
@@ -12,6 +12,29 @@ print(tupla)
 
 print(tupla[1])
 
-tupla[0] = 100
+#tupla[0] = 100  #Erro, pois não é possivel alterar uma tupla
+
+#Manipulando dicionários:
+dic = {"semMundial" : "Palmeiras", "1mundial" : "Corinthians", "2mundiais" : "SãoPaulo"}
+
+print(dic["semMundial"])
+
+notas = {"mat":5, "lp":10, "ef":8}
+
+print(notas)
+print(notas["lp"])
+
+#print(notas["bio"])
+
+print(dir(notas))
+print()
+print(notas.values())
+print()
+print(notas.keys())
+print()
+print(len(notas))
+
+for disciplina in notas.keys():
+    print(disciplina)
 
 time.sleep(3)
